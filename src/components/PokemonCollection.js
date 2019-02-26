808c2603492b555bb8fa9fe9a8d9bb2ff2231f7d
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-26 08:36:49 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
+// import { prependOnceListener } from 'cluster';
 
-class PokemonCollection extends React.Component {
-  render() {
+const PokemonCollection = (props) => {
+  const pokeList = props.pokemon.map(poke =>
+    <PokemonCard
+      name={poke.name}
+      sprites={poke.sprites}
+      hp = {poke.stats.pop()} />)
+
     return (
-      <Card.Group itemsPerRow={6}>
+      <div>
         <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
+          <Card.Group itemsPerRow={6}>
+            {pokeList}
+          </Card.Group>
+      </div>
     )
-  }
+
 }
 
 export default PokemonCollection
